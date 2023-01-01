358cba7c8c0c241d0f394566405693ba6df4375e
mariva <mariva.yoga@gmail.com>
2023-01-01 22:54:24 +0300

    main2

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
 from const import *
+from menu import *
+
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode(size)
+    start_screen(screen)
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
