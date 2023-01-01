d7a3c2ce49c1cc16d0a64bfa43f8db3c357fcb38
mariva <mariva.yoga@gmail.com>
2023-01-01 22:01:25 +0300

    items1

---
+++ Items.py
@@ -0,0 +1,26 @@
+class Cofe:
+    ...
+
+
+class MiniCofe(Cofe):
+    ...
+
+
+class StandartCofe(Cofe):
+    ...
+
+
+class BigCofe(Cofe):
+    ...
+
+
+class Glasses:
+    ...
+
+
+class Cap:
+    ...
+
+
+class Knife:
+    ...
