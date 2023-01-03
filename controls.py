f0e35307a9eb54f0ac5dddbd2239de2deda2eb0c
mariva <mariva.yoga@gmail.com>
2023-01-03 17:48:24 +0300

    controls1

--- controls.py
+++ controls.py
@@ -6,3 +6,12 @@ def event():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                pass
+            elif event.key == pygame.K_DOWN:
+                pass
+            elif event.key == pygame.K_LEFT:
+                pass
+            elif event.key == pygame.K_RIGHT:
+                pass
