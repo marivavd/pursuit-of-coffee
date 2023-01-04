a4b253368d5c672b319ac9113463c5148b322ece
mariva <mariva.yoga@gmail.com>
2023-01-04 09:12:38 +0300

    animals with masks

--- animals.py
+++ animals.py
@@ -10,15 +10,6 @@ class Animal:
         self.y = 430
         self.max_y = 420
         self.min_y = 470
-        self.flag_jump = False
-        self.flag_rise = False
-
-    def jump(self):
-        self.y -= 5 if self.flag_rise else -5
-        if self.y < self.max_y:
-            self.flag_rise = False
-        if self.y > self.min_y:
-            self.flag_jump = False
 
     def shift_side(self, k=1):
         shift = 12 * k
@@ -32,12 +23,16 @@ class Raccoon(Animal):
     def __init__(self):
         super(Raccoon, self).__init__()
         self.img = pygame.transform.scale(load_image('raccoon.png'), (width // 6, height // 6))
+        self.rect = self.img.get_rect()
+        self.mask = pygame.mask.from_surface(self.img)
 
 
 class Hedgehog(Animal):
     def __init__(self):
         super(Hedgehog, self).__init__()
         self.img = pygame.transform.scale(load_image('hedgehog.png'), (width // 6, height // 6))
+        self.rect = self.img.get_rect()
+        self.mask = pygame.mask.from_surface(self.img)
 
 
 class Goose(Animal):
