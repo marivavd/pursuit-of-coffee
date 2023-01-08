7c510a94ddd794d224d842b41c68c2222d75147f
Nikita <Entvk20071@mail.ru>
2023-01-08 17:13:47 +0300

    refactor const

--- const.py
+++ const.py
@@ -4,11 +4,13 @@ size = width, height = 800, 600
 FPS = 50
 period = [10, 10]
 clock = pygame.time.Clock()
-ground_level = 500
-track_width = 12
 all_obstacles = pygame.sprite.Group()  # все препятствия
 things = pygame.sprite.Group()  # кепка и очки
 weapon = pygame.sprite.Group()  # нож и мина
 cofe = pygame.sprite.Group()  # кофе
-groups = [all_obstacles, things, weapon, cofe]
-sp_fons = ['fon.jpg', 'fon1.jpg']
+groups = (all_obstacles, things, weapon, cofe)
+
+sl_fons = {'fon.jpg': {'ground_level': 500,
+                       'track_width': 12},
+           'fon1.jpg': {'ground_level': 500,
+                        'track_width': 12}}
--- controls.py
+++ controls.py
@@ -14,35 +14,38 @@ class Event:
         self.throw_knife = []
         self.mina_time = []
 
-    def proverka_event(self, hero):
+    def check_event(self, hero):
         # обработка событий
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    self.isjump = True
-                elif event.key == pygame.K_LEFT:
-                    hero.shift_side(-1)
-                elif event.key == pygame.K_RIGHT:
-                    hero.shift_side()
-                elif event.key == pygame.K_DOWN:  # оставлять мину
-                    if self.mina > 0:
-                        self.mina_time.append([hero.x, hero.y + 40, time.perf_counter()])
-                        self.mina -= 1
+                self.check_key(event, hero)
                 # сделать так, чтобы при смене персонажа, и нож, и мина оставались у него
-                elif event.key == pygame.K_SPACE:  # пулять ножом во врага
-                    if self.knife > 0:
-                        self.throw_knife.append(pygame.transform.scale(load_image('knife.png', -1), (100, 100)).get_rect(
-                            topleft=(hero.x - 30, hero.y)))
-                        self.knife -= 1
-                        if self.knife == 0:
-                            if hero.name == 'raccoon':
-                                hero.img = pygame.transform.scale(load_image('raccoon.png'), (width // 6, height // 6))
-                            elif hero.name == 'hedgehog':
-                                hero.img = pygame.transform.scale(load_image('hedgehog.png'), (width // 6, height // 6))
-                            else:
-                                hero.img = pygame.transform.scale(load_image('goose.png'), (width // 6, height // 6))
+
+    def check_key(self, event, hero):
+        if event.key == pygame.K_UP:
+            self.isjump = True
+        elif event.key == pygame.K_LEFT:
+            hero.shift_side(-1)
+        elif event.key == pygame.K_RIGHT:
+            hero.shift_side()
+        elif event.key == pygame.K_DOWN:  # оставлять мину
+            if self.mina > 0:
+                self.mina_time.append([hero.x, hero.y + 40, time.perf_counter()])
+                self.mina -= 1
+        elif event.key == pygame.K_SPACE:  # пулять ножом во врага
+            if self.knife > 0:
+                self.throw_knife.append(pygame.transform.scale(load_image('knife.png', -1), (100, 100)).get_rect(
+                    topleft=(hero.x - 30, hero.y)))
+                self.knife -= 1
+                if self.knife == 0:
+                    if hero.name == 'raccoon':
+                        hero.img = pygame.transform.scale(load_image('raccoon.png'), (width // 6, height // 6))
+                    elif hero.name == 'hedgehog':
+                        hero.img = pygame.transform.scale(load_image('hedgehog.png'), (width // 6, height // 6))
+                    else:
+                        hero.img = pygame.transform.scale(load_image('goose.png'), (width // 6, height // 6))
 
     def proverka_contact(self, hero, all_obstacles, things, weapon, cofe):
         for i in all_obstacles:  # проверка на соприкосновение с препятствием
