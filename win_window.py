a643721a3b6a8cc68080630419c679181c90ca21
mariva <mariva.yoga@gmail.com>
2023-01-28 08:51:28 +0300

    win_window is done

--- win_window.py
+++ win_window.py
@@ -1,6 +1,9 @@
 import time
 from time import perf_counter
 from map import time
+from const import load_image
+from random import choice
+from magic import magic
 from const import pygame, size, width, height
 
 pygame.init()
@@ -9,16 +12,19 @@ screen_rect = (0, 0, width, height)
 
 
 def init_intro_text(intro_text, text_coord=50):
-    font = pygame.font.Font('fonts/Paper.otf', 100)
+    font = pygame.font.Font('fonts/20216.ttf', 100)
+    top = -30
     for n, line in enumerate(intro_text):
-        string_rendered = font.render(line, True, (255, 0, 0))
+        string_rendered = font.render(line, True, (255, 255, 0))
         font = pygame.font.Font(None, 40)
-        intro_rect = string_rendered.get_rect()
         text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+        text_x = width // 2 - string_rendered.get_width() // 2
+        text_y = height // 2 - string_rendered.get_height() // 2 + top
+        top += 30
+        text_w = string_rendered.get_width()
+        text_h = string_rendered.get_height()
+        screen.blit(string_rendered, (text_x, text_y))
+
 
 
 def open_victory_window():
@@ -33,6 +39,9 @@ def open_victory_window():
     f.close()
     clock = pygame.time.Clock()
     running = True
+    sound = pygame.mixer.Sound(f'sounds/fanfara.mp3')
+    sound.play()
+    magic()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -43,5 +52,6 @@ def open_victory_window():
                          f'Общее количество ваших побед равно {count_win}'))
         pygame.display.flip()
         clock.tick(50)
-        if perf_counter() - time_begin >= 3:
+        if perf_counter() - time_begin >= 7:
             running = False
+open_victory_window()
