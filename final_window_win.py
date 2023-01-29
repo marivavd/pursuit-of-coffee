e64f98203c4403a4b1040e6c3a730a4a586cdeb1
mariva <mariva.yoga@gmail.com>
2023-01-29 16:34:34 +0300

    win window is done

--- final_window_win.py
+++ final_window_win.py
@@ -8,35 +8,31 @@ screen_rect = (0, 0, width, height)
 
 
 def init_intro_text(intro_text, text_coord=50):
-    font = pygame.font.Font('fonts/Paper.otf', 100)
+    font = pygame.font.Font('fonts/Zaychik-Regular.ttf', 80)
     top = -30
     for n, line in enumerate(intro_text):
-        string_rendered = font.render(line, True, (200, 0, 0))
-        font = pygame.font.Font(None, 40)
+        string_rendered = font.render(line, True, (255, 105, 0))
+        font = pygame.font.Font('fonts/Zaychik-Regular.ttf', 60)
         text_coord += 10
         text_x = width // 2 - string_rendered.get_width() // 2
         text_y = height // 2 - string_rendered.get_height() // 2 + top
-        top += 30
-        text_w = string_rendered.get_width()
-        text_h = string_rendered.get_height()
+        top += 100
         screen.blit(string_rendered, (text_x, text_y))
 
 
-def open_loss_window(time_pl):
+def open_win_window():
     time_begin = perf_counter()
-    time_of_game = int(time_begin - time_pl)
     clock = pygame.time.Clock()
     running = True
-    sound = pygame.mixer.Sound(f'sounds/death_music.mp3')
+    sound = pygame.mixer.Sound(f'sounds/breath.mp3')
     sound.play()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
-        init_intro_text(("Game is over", "", "", "", '', f'Вы играли {time_of_game} секунд'))
-        screen.blit(pygame.transform.scale(load_image('blood.png'), (width // 3, height // 3)), (100, 10))
-        screen.blit(pygame.transform.scale(load_image('grave.png'), (173, 213)), (550, 350))
+        screen.fill((10, 50, 40))
+        init_intro_text(("You are still breathing", 'You are lucky'))
+        screen.blit(pygame.transform.scale(load_image('great work.png'), (256, 256)), (550, 350))
         pygame.display.flip()
         clock.tick(50)
         if perf_counter() - time_begin >= 5:
